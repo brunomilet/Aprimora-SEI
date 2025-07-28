547772d5b8d3d706bd4d8b244ef2d815ba1c163a
Bruno Milet <brunomgrj@gmail.com>
2025-07-28 20:24:33 -0300

    Retirada dos console.log

--- seiClient.js
+++ seiClient.js
@@ -2,13 +2,7 @@ require('dotenv').config();
 const soap = require('soap');
 
 async function createClient() {
-  console.log('Criando cliente SOAP para SEI...');
-  console.log('Conectando ao WSDL do SEI:', process.env.SEI_WSDL_URL);
   const client = await soap.createClientAsync(process.env.SEI_WSDL_URL);
-  console.log('Cliente SOAP criado com sucesso');
-  //client.addHttpHeader('Content-Type', 'text/xml;charset=UTF-8');
-  //client.addHttpHeader('SOAPAction', '""');
-  //console.erro('Erro ao criar cliente SOAP:', erro);
   return client;
 }
 
